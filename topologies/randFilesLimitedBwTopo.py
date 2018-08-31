59964eba598bdd5ed79258f853079c950449025f
Mattia De Prisco <mattia.dipi@gmail.com>
2018-08-31 12:23:24 +0200

    Update randFilesLimitedBwTopo.py

--- topologies/randFilesLimitedBwTopo.py
+++ topologies/randFilesLimitedBwTopo.py
@@ -14,6 +14,9 @@ class RandomFilesHost( Node ):
 
 	def config( self, **params ):
 		super( RandomFilesHost, self).config( **params )
+		
+		
+		# Create random files
 		self.cmd("mkdir /var/www/html/ipsec")
 		self.cmd("openssl rand -out /var/www/html/ipsec/10K 10000")
 		self.cmd("openssl rand -out /var/www/html/ipsec/100K 100000")
@@ -21,6 +24,7 @@ class RandomFilesHost( Node ):
 		self.cmd("openssl rand -out /var/www/html/ipsec/10M 10000000")
 		self.cmd("openssl rand -out /var/www/html/ipsec/100M 100000000")
 		
+		# Create private /etc directory
 		etc = '/tmp/etc-%s' % self.name
 		self.cmd( 'mkdir -p', etc )
 		self.cmd( 'mount --bind /etc', etc )
@@ -30,8 +34,11 @@ class RandomFilesHost( Node ):
 		self.cmd( 'cp -a /%s/. /etc/' % etc )
 
 	def terminate( self ):
+		
+		# Remove randomly generated files
 		self.cmd("rm -r /var/www/html/ipsec")
 		
+		# Remove temporary /etc directory
 		etc = '/tmp/etc-%s' % self.name
 		self.cmd( 'umount /etc' )
 		self.cmd( 'umount', etc )
@@ -40,10 +47,12 @@ class RandomFilesHost( Node ):
 
 class LimitedBwTopo(Topo):
     
-	"N hosts connected through a switch"
+	# Two hosts connected through a switch, with limited bandwidth.
+	# Alice host has random generated files under /var/www/html/ipsec/
 
 	def build(self, bandwidth=5):
 	
+		# Configure bandwidth value
 		bandwidth=float(bandwidth)
 		if(bandwidth <= 0):
 			print "*** Bandwidth can not be negative. Setting it to the minimum value ***"
@@ -57,19 +66,23 @@ class LimitedBwTopo(Topo):
 		hostAlice = self.addHost('alice', ip='10.0.0.1/24', cls=RandomFilesHost, mac='00:00:00:00:00:01')
 		hostBob = self.addHost('bob', ip='10.0.0.2/24', cls=PrivateEtcHost, mac='00:00:00:00:00:02')
 	
+		# Add links
 		self.addLink( centralSwitch, hostAlice, bw=bandwidth)
 		self.addLink( centralSwitch, hostBob, bw=bandwidth)
         
 def limitedBwTopo(): 
 	if(len(sys.argv) > 1):
+		
+		# Check if argument is a valid float 
 		try:
 			float(sys.argv[1])
 			topo = LimitedBwTopo(sys.argv[1])
 		except ValueError:
-			print "*** Ignoring argument, only numbers are valid ***"
+			print "*** Wrong parameters. Usage: python randFilesLimitedBwTopo.py [b] - with b float value of links' bandwidth. ***"
 			topo = LimitedBwTopo()
 	else:
 		topo = LimitedBwTopo()
+	
 	net = Mininet(topo, link=TCLink)
 	net.start()
 	CLI(net)
