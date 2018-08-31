3efc377322c53f068551a252e31271de9d1e1506
Mattia De Prisco <mattia.dipi@gmail.com>
2018-08-31 12:20:32 +0200

    Update simpleTopo.py

--- topologies/simpleTopo.py
+++ topologies/simpleTopo.py
@@ -12,6 +12,7 @@ class SimpleTopo(Topo):
     
 	"N hosts connected through a switch, each with private /etc directories"
   
+	# Add Alice and Bob hosts
 	def addAliceBob(self, hostGroup):
 		hostAlice = self.addHost('alice', cls=PrivateEtcHost, ip='10.0.0.1/24', mac='00:00:00:00:00:01')
 		hostBob = self.addHost('bob', cls=PrivateEtcHost, ip='10.0.0.2/24', mac='00:00:00:00:00:02')
@@ -26,6 +27,8 @@ class SimpleTopo(Topo):
 		centralSwitch = self.addSwitch('s1')
 		hostGroup = []
 		if(n <= 2):
+			if(n < 2):
+				print "*** Minimum number of hosts is two. Instantiating two hosts. ***
 			self.addAliceBob(hostGroup)
 		elif(n == 3):
 		       	self.addAliceBob(hostGroup)
@@ -36,6 +39,8 @@ class SimpleTopo(Topo):
 				host = self.addHost('h%d' %k, cls=PrivateEtcHost, ip='10.0.0.%d/24' %k, mac='%s' %hex(k)[2:].zfill(12))
 				hostGroup.append(host)
 		elif(n >= 253):
+			if(n > 253)
+				print "*** Maximum number of hosts is 253. Instantiating 253 hosts. ***
 			for k in range(1,253):
 				host = self.addHost('h%d' %k, cls=PrivateEtcHost, ip='10.0.0.%d/24' %k, mac='%s' %hex(k)[2:].zfill(12))
 				hostGroup.append(host)
@@ -45,13 +50,19 @@ class SimpleTopo(Topo):
 			self.addLink( centralSwitch, h)  
        
 def simpleTopo(): 
+	
+	# Check arguments passed
 	if(len(sys.argv) > 1 and sys.argv[1].isdigit()):
 		topo = SimpleTopo(sys.argv[1])
 	else:
 		topo = SimpleTopo()
+
 	net = Mininet( topo )
 	if((len(sys.argv) == 2 and sys.argv[1] == 'c') or (len(sys.argv) > 2 and sys.argv[2] == 'c')):
 		net.addNAT().configDefault()
+	else:
+		print "*** Wrong parameters. Usage: python simpleTopo.py [n] [c] - with n number of hosts and c to give internet access to the topology. ***"
+	
 	net.start()
 	CLI(net)
 	net.stop()
