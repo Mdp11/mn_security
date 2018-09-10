140eda11aea8e6f9dd3d4777f5f556fafa635f7b
Mattia De Prisco <mattia.dipi@gmail.com>
2018-09-10 18:39:58 +0200

    Create ddos.py

---
+++ topologies/ddos.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import Node
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+import sys
+
+class SimpleTopo(Topo):
+    
+	"100 hosts connected through a switch"
+  
+	def build(self, n=2):
+	
+		n=int(n)
+	
+		# Add hosts and switch
+		rightSwitch = self.addSwitch('s1')
+		leftSwitch = self.addSwitch('s2')
+		hostGroup = []
+		attacker = self.addHost('alice', ip='10.0.0.101/24', mac='00:00:00:11:11:11')
+		victim = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+		for k in range(1,n+1):
+			host = self.addHost('h%d' %k, ip='10.0.0.%d/24' %k, mac='%s' %hex(k)[2:].zfill(12))
+			hostGroup.append(host)
+
+		# Add links
+		self.addLink(centralSwitch, hostAlice)
+		self.addLink(centralSwitch, outerSwitch)
+		self.addLink(outerSwitch, hostH1)
+		for h in hostGroup:
+			self.addLink( centralSwitch, h)  
+       
+def simpleTopo(): 
+	
+	# Check arguments passed
+	
+	nHosts = False
+	
+	if(len(sys.argv) > 1 and sys.argv[1].isdigit()):
+		topo = SimpleTopo(sys.argv[1])
+		nHosts = True
+	else:
+		topo = SimpleTopo()
+
+	net = Mininet( topo)
+	net.start()
+	CLI(net)
+	net.stop()
+
+
+if __name__ == '__main__':
+	setLogLevel( 'info' )
+	simpleTopo()
