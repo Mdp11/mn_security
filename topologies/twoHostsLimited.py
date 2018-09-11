794a396e07275c3357cf969a5b433a1f53854daf
Mattia De Prisco <mattia.dipi@gmail.com>
2018-09-11 13:03:05 +0200

    Create twoHostsLimited.py

---
+++ topologies/twoHostsLimited.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import Node, CPULimitedHost
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+import sys
+
+class TwoHostsLimited(Topo):
+    
+	"2 hosts connected through a switch, with limited CPU"
+
+	def build(self):
+
+	
+		# Add hosts and switch
+		centralSwitch = self.addSwitch('s1')
+		hostAlice = self.addHost('alice', cls=CPULimitedHost, ip='10.0.0.1/24', mac='00:00:00:00:00:01', cpu=.05)
+		hostBob = self.addHost('bob', cls=CPULimitedHost, ip='10.0.0.2/24', mac='00:00:00:00:00:02', cpu=.3)
+		#hostAlice = self.addHost('alice', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+		#hostBob = self.addHost('bob', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
+
+		# Add links
+		self.addLink( centralSwitch, hostAlice)  
+		self.addLink( centralSwitch, hostBob)  
+       
+def twoHostsLimited(): 
+
+	topo = TwoHostsLimited()
+	net = Mininet( topo=topo, host=CPULimitedHost )
+	net.start()
+	CLI(net)
+	net.stop()
+
+
+if __name__ == '__main__':
+	setLogLevel( 'info' )
+	twoHostsLimited()
