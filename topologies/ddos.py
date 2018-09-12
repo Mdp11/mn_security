c8b9236bc7a6d8e9a8555b4c6419057208d85d20
Mattia De Prisco <mattia.dipi@gmail.com>
2018-09-12 13:54:28 +0200

    Update ddos.py

--- topologies/ddos.py
+++ topologies/ddos.py
@@ -2,7 +2,7 @@
 
 from mininet.net import Mininet
 from mininet.topo import Topo
-from mininet.node import Node
+from mininet.node import Node, CPULimitedHost
 from mininet.cli import CLI
 from mininet.log import setLogLevel
 import sys
@@ -14,18 +14,18 @@ class SimpleTopo(Topo):
 	def build(self, n=2):
 	
 		n=int(n)
-	
+		
+		if(n>100):
+			n=100
 		# Add hosts and switch
 		centralSwitch = self.addSwitch('s1')
 		hostGroup = []
-		hostAlice = self.addHost('alice', ip='10.0.0.101/24', mac='00:00:00:11:11:11')
-		h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+		h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01', cpu=.001)
 		for k in range(2,n+1):
 			host = self.addHost('h%d' %k, ip='10.0.0.%d/24' %k, mac='%s' %hex(k)[2:].zfill(12))
 			hostGroup.append(host)
 
 		# Add links
-		self.addLink(centralSwitch, hostAlice)
 		self.addLink(centralSwitch, h1)
 		for h in hostGroup:
 			self.addLink( centralSwitch, h)  
