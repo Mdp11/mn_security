cc49a71c63cba85e4ae7ae688f0a88a4d905c76a
Mattia De Prisco <mattia.dipi@gmail.com>
2018-09-16 15:56:03 +0200

    Update clusterVM1.py

--- topologies/clusterVM1.py
+++ topologies/clusterVM1.py
@@ -22,10 +22,8 @@ def vm1Net():
 
 	#Set up hosts, switch and links
 	hostAlice = net.addHost( 'alice', cls=PrivateEtcHost, ip='10.0.0.1/24', mac='00:00:00:00:00:01' )
-	hostChuck = net.addHost('chuck', cls=PrivateEtcHost, ip='10.0.0.3/24', mac='00:00:00:00:00:03')
 	s1 = net.addSwitch( 's1' )
 	net.addLink( hostAlice, s1 )
-	net.addLink( hostChuck, s1 )
 
 	# Delete old tunnel if it still exists
 	s1.cmd('ifconfig s1-gre1 down')
