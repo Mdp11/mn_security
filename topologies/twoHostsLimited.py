b3e9286fab37741d6af019d7703680ca879b8de5
Mattia De Prisco <mattia.dipi@gmail.com>
2018-09-18 17:31:20 +0200

    Update twoHostsLimited.py

--- topologies/twoHostsLimited.py
+++ topologies/twoHostsLimited.py
@@ -9,15 +9,18 @@ import sys
 
 class TwoHostsLimited(Topo):
     
-	"2 hosts connected through a switch, one with 5% of the system CPU, the other with the 50%."
+	# 2 hosts connected through a switch
+	# Alice with 5% of the system CPU, Bob with the 50%
 
 	def build(self):
 
 	
 		# Add hosts and switch
 		centralSwitch = self.addSwitch('s1')
-		hostAlice = self.addHost('alice', cls=CPULimitedHost, ip='10.0.0.1/24', mac='00:00:00:00:00:01', cpu=.05)
-		hostBob = self.addHost('bob', cls=CPULimitedHost, ip='10.0.0.2/24', mac='00:00:00:00:00:02', cpu=.5)
+		hostAlice = self.addHost('alice', ip='10.0.0.1/24', mac='00:00:00:00:00:01',
+					 cls=CPULimitedHost, cpu=.05)
+		hostBob = self.addHost('bob', ip='10.0.0.2/24', mac='00:00:00:00:00:02',
+				       cls=CPULimitedHost, cpu=.5)
 
 		# Add links
 		self.addLink( centralSwitch, hostAlice)  
