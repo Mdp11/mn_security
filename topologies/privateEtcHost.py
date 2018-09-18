0e41571ad12ba1c6330fcbb6850b3aa5f4dc9fcc
Mattia De Prisco <mattia.dipi@gmail.com>
2018-09-18 18:03:37 +0200

    Update privateEtcHost.py

--- topologies/privateEtcHost.py
+++ topologies/privateEtcHost.py
@@ -7,9 +7,18 @@ class PrivateEtcHost( Node ):
 		super( PrivateEtcHost, self).config( **params )
 		etc = '/tmp/etc-%s' % self.name
 		self.cmd( 'mkdir -p', etc )
+		
+		# Bind the /etc folder to the /tmp/etc-hostName folder just created
+		# so the same content is accessible in it
 		self.cmd( 'mount --bind /etc', etc )
+		
+		# Mount a temporary fylesystem at the /etc directory
 		self.cmd( 'mount -n -t tmpfs tmpfs /etc' )
+		
+		# Create links to each file from the /tmp/etc-hostName folder
+		# in the /etc folder
 		self.cmd( 'ln -s %s/* /etc/' % etc )
+		
 		self.cmd( 'rm -rf /etc/*' )
 		self.cmd( 'cp -a /%s/. /etc/' % etc )
 
